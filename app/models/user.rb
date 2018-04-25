f053edb0775487283bbce441b9e63a2bba4cb257
github username <diep.christopher@gmail.com>
2018-04-25 14:44:21 -0400

    updated user model with secure password and other validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,7 @@ class User < ApplicationRecord
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :email, presence: true
-  
+  validates :email, uniqueness: true
+  has_secure_password
+
 end
