9eb1cec4e5a4a2925d65f43fcb0e56c06563caed
github username <diep.christopher@gmail.com>
2018-04-25 13:35:10 -0400

    made routes for user and seeding db to test

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   #routes for API backend
-  # namespace :api do
-  #   namespace :v1 do
-  #     resources :notes, only: [:index, :update]
-  #   end
-  # end
+  namespace :api do
+    namespace :v1 do
+      resources :users
+    end
+  end
 end
