8e5f0b92e484fd315f10fe52b6cbe12c52d642c2
github username <diep.christopher@gmail.com>
2018-04-26 10:52:16 -0400

    cleaned up auth controller comments

--- app/controllers/api/v1/auth_controller.rb
+++ app/controllers/api/v1/auth_controller.rb
@@ -1,11 +1,11 @@
 class Api::V1::AuthController < ApplicationController
   # #anyone can view this page. Just the show and create pages
   skip_before_action :authorized, only: [:create, :show]
-  #
+
   def create
     @user = User.find_by(email: user_login_params[:email])
 
-    #if password matches check token
+    #if user login matches. jwt token created and given
     if @user && @user.authenticate(user_login_params[:password])
       token = encode_token({ user_id: @user.id })
       render json: { email: @user.email, jwt: token }, status: 202
@@ -13,10 +13,10 @@ class Api::V1::AuthController < ApplicationController
       render json: { message: "Invalid email or password" }, status: 401
     end
   end
-  #
+
   def show
     #Found user. then make jwt token encoded
-    if !!current_user #current_user comes from application controller; it finds current user by id found in decoded JWT token
+    if !!current_user #current_user comes from application controller; is current user loggined? show page
       token = encode_token({ user_id: current_user.id })
       render json: { email: current_user.email, jwt: token }, status: 200
     else
