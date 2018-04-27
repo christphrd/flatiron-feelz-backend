cf38e60736ffaf7de36e6c5d5e7f2b19f93ef987
github username <diep.christopher@gmail.com>
2018-04-26 22:37:33 -0400

    resourceful routing for posts model

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
       post "/login", to: "auth#create"
       post "/signup", to: "users#create"
       get "/current_user", to: "auth#show"
+      resources :posts
     end
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -10,3 +10,5 @@ user1 = User.create(first_name: "911", last_name: "911", email: "911", password:
 user2 = User.create(first_name: "123", last_name: "123", email: "123", password: "123", password_confirmation: "123")
 user3 = User.create(first_name: "Chris", last_name: "Diep", email:"chris", password: "chris", password_confirmation: "chris")
 post1 = Post.create(user_id: 1, feelings: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.")
+post2 = Post.create(user_id: 1, feelings: "Awesome...nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.")
+post3 = Post.create(user_id: 1, feelings: "=(")
