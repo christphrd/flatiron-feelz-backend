b453d3fc6cc7fedbd4abfe80367b411110a310b0
github username <diep.christopher@gmail.com>
2018-04-29 12:22:59 -0400

    updated posts controller to send json with status for successful post creation

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -5,9 +5,9 @@ class Api::V1::PostsController < ApplicationController
     @post = Post.new(post_params)
     if @post.valid?
       @post.save
-      render json: { user_id: @post.user_id, feelings: @post.feelings , dog_spirit: @post.dog_spirit }, status: 202
+      render json: { user_id: @post.user_id, feelings: @post.feelings , dog_spirit: @post.dog_spirit, status: 202 }
     else
-      render json: { message: "#{@post.errors.messages.keys[0]} #{@post.errors[@post.errors.messages.keys[0]][0]}" }, status: 404
+      render json: { message: "#{@post.errors.messages.keys[0]} #{@post.errors[@post.errors.messages.keys[0]][0]}", status: 404 }
     end
   end
 
