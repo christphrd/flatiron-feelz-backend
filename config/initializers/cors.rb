b7371bd17030b631b1fdeee0c32ee5424813fefd
github username <diep.christopher@gmail.com>
2018-05-09 14:05:11 -0400

    limit frontend with cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,8 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '*'
+    # origins '*'
+    origins 'https://floating-wildwood-28213.herokuapp.com'
 
     resource '*',
       headers: :any,
