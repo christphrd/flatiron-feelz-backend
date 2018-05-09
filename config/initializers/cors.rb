06834bb33d57eb062b831dba5e605f9409728fc1
github username <diep.christopher@gmail.com>
2018-05-09 17:44:45 -0400

    Revert to 98d9459

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,8 +7,8 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    # origins '*'
-    origins 'thawing-reef-19932.herokuapp.com'
+    origins '*'
+    # origins 'https://thawing-reef-19932.herokuapp.com/'
 
     resource '*',
       headers: :any,
