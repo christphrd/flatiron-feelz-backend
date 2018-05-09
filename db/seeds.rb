f5a64142609ef560509f27ce4477cabbbb4b3e39
github username <diep.christopher@gmail.com>
2018-05-09 01:11:04 -0400

    no more seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,11 +5,3 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-
-user1 = User.create(first_name: "911", last_name: "911", email: "911", password: "911", password_confirmation: "911")
-user2 = User.create(first_name: "123", last_name: "123", email: "123", password: "123", password_confirmation: "123")
-user3 = User.create(first_name: "Chris", last_name: "Diep", email:"chris", password: "chris", password_confirmation: "chris")
-post1 = Post.create(user_id: 1, feelings: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.")
-post2 = Post.create(user_id: 1, feelings: "Awesome...nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.")
-post3 = Post.create(user_id: 1, feelings: "=(")
-selfie1 = Selfie.create(user_id: 1, link: "https://i.pinimg.com/originals/14/0f/9f/140f9fa891dd5c2e1e5c3faa3360c570.jpg", anger: 0.0, contempt: 0.041, disgust: 0.0, fear: 0.0, happiness: 0.678, neutral: 0.281, sadness: 0.0, surprise: 0.0)
