25a24465b17a40d6a622afe2b6083867f201f36e
github username <diep.christopher@gmail.com>
2018-05-29 00:48:16 -0400

    cors test

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,8 +7,8 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    # origins '*'
-    origins 'https://flatironfeelz.herokuapp.com'
+    origins '*'
+    # origins 'https://flatironfeelz.herokuapp.com'
 
     resource '*',
       headers: :any,
