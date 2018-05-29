215dfeedf318a1f6bb576d041d9af0cbd2d96df5
github username <diep.christopher@gmail.com>
2018-05-29 00:43:10 -0400

    cors.rb origins update

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -8,7 +8,7 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     # origins '*'
-    origins 'flatironfeelz.herokuapp.com'
+    origins 'https://flatironfeelz.herokuapp.com'
 
     resource '*',
       headers: :any,
